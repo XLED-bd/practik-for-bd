39a2b73f542b75fa027c1f1aa6a5fc86e079e287
Ivan <korol.gorox@gmail.com>
2024-05-27 23:48:45 +0300

    Add connection with db

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,59 @@ namespace test
 {
     public partial class ClientsForm : Form
     {
+        private string connectionString = "Host=localhost:5432;Username=postgres;Password=root;Database=bank";
+        private int status = 0;
+
         public ClientsForm()
         {
             InitializeComponent();
+            LoadDataWithIndex();
+        }
+
+        private void LoadDataWithIndex()
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new NpgsqlCommand("SELECT * FROM users;", conn);
+                var reader = cmd.ExecuteReader();
+
+                var dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void LoadDataWithoutIndex()
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new NpgsqlCommand("SELECT first_name, second_name, birth_date, phone, passport, snils, inn, email, password FROM users;", conn);
+                var reader = cmd.ExecuteReader();
+
+                var dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (status == 0)
+            {
+                LoadDataWithoutIndex(); button1.Text = "Показать индексы"; ++status;
+            }
+            else { LoadDataWithIndex(); button1.Text = "Убрать индексы"; --status; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var mainForm = new MainFormEmployee();
+            mainForm.Show();
+            this.Hide();
         }
     }
 }
